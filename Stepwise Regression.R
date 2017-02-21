7b3be36a9486b333242d9c4ac7681300a35cbd3c
iGuest <Tim Pace>
2017-02-20 20:42:43 -0800

    Initial submit

--- Stepwise Regression.R
+++ Stepwise Regression.R
@@ -7,9 +7,12 @@ getwd()
 data <- read.csv("globalterrorismdb_0616dist_US_ONLY.csv")
 head(data)
 
+nrow(data)
+str(data)
+
 fit <- lm(success ~ nkill + nkillus + nwoundus + nperps + nperpcap + nkillter +
             nwound + nwoundte + attacktype1 + targtype1 +
-            targsubtype1 + weaptype1 + weapsubtype1,data=data)
+            targsubtype1 + weaptype1 + weapsubtype1, data=data)
 
 # attacktype1_txt + targtype1_txt +
 # targsubtype1_txt + weaptype1_txt + weapsubtype1_txt
@@ -17,3 +20,7 @@ fit <- lm(success ~ nkill + nkillus + nwoundus + nperps + nperpcap + nkillter +
 
 step <- stepAIC(fit, direction="both")
 step$anova # display results
+
+plot(step)
+
+plot(data$nkillus, data$nkill, xlim=c(0,50), ylim=c(0,20), size=data$nkill)
