bf6e1f7879ae50ba6b481433152ddedc3634f6ce
Boris Pavlov <pboris84@gmail.com>
2017-04-18 08:44:51 -0700

    Stepwise Update

--- plots.R
+++ plots.R
@@ -17,3 +17,13 @@ gg.success.weap <- ggplot(gtd, aes(weaptype1_txt, nkill))
 gg.success.weap + geom_bar(stat="identity") + ylab("Number of Successful Attacks") +
   ggtitle("Number of Casualties per Weapon Type 1970-2015") + xlab("Weapon Type") +
   scale_x_discrete(limits=weap.positions)
+
+# plot attacks by attack type in the 48 contiguous united states
+us <- map_data("state")
+q <- ggplot() +
+geom_polygon(data=us, aes(x=long, y=lat, group=group), color="darkgray", fill="white") +
+geom_point(data=gtd, aes(x=longitude, y=latitude, color=attacktype1_txt), size=2, alpha=0.5) +
+coord_map() + xlim(-125, -65) + ylim(25, 50) + xlab("Longitude") + ylab("Latitude") +
+theme_bw() + theme(legend.position="bottom", legend.title=element_blank()) +
+ggtitle("Terrorist Attacks in the United States, by Attack Type (1970-2015)")
+q
